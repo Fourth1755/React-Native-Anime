d84657597e7df7ed4b8ebbca71de13480dc621ee
Fourth1755 <fourthe.hero@gmail.com>
2022-02-19 23:01:42 +0700

    เพิ่ม

--- App.js
+++ App.js
@@ -9,9 +9,10 @@ import Box2 from './components/Box2';
 import Test from './components/Test';
 import AppLoading from 'expo-app-loading';
 import Main from './components/Main';
-import TestVideo from './components/TestVideo';
 import { useFonts, Prompt_400Regular,Prompt_500Medium } from '@expo-google-fonts/prompt';
-
+import {NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TestNavigation from './components/TestNavigation';
 const App=()=> {
   let [fontsLoaded] = useFonts({
     Prompt_400Regular,
@@ -21,19 +22,12 @@ const App=()=> {
     return <AppLoading />;
   } else {
     return (
-    <View style={styles.container}>
-      <Header/>
-      <Main/>
-      <Datalist/>
-      <StatusBar style="auto"/>
-    </View>
+      <TestNavigation/>
   );
   }
 }
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    paddingTop:40,
     fontFamily:'Prompt_500Medium'
   },
 });
